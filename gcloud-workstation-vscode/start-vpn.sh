8e3123a4ac4410b18c3b741f91d704effb0915be
Mickael Saavedra <mickael.saavedra@strg.at>
2024-01-10 13:33:32 +0100

    feat: Update vpn startup script, INPRO-2035

--- gcloud-workstation-vscode/start-vpn.sh
+++ gcloud-workstation-vscode/start-vpn.sh
@@ -1,14 +1,19 @@
 #/bin/bash
-OVPN_CONFIG_FILE=~/openvpn-client.ovpn
+# This script is run automatically by root in the container.
+# Added sudo in case it needs to be run by the unprivileged user.
+OVPN_CONFIG_FILE=/home/user/openvpn-client.ovpn
+
 # Killing any openvpn connections in case this script is used when openvpn processes are already running
 sudo killall openvpn 2>/dev/null
 
 # We check if the OpenVPN configuration file is present. If it is
 if test -f "$OVPN_CONFIG_FILE"; then
     echo "OpenVPN configuration file has been found, attempting connection."
-    sudo openvpn ~/openvpn-client.ovpn > /tmp/openvpn.log 2>&1 &
+    sudo openvpn /home/user/openvpn-client.ovpn > /tmp/openvpn.log 2>&1 &
+
     # Giving time to openvpn to try and connect (the openvpn command does not exit upon successful connection)
     sleep 5
+
     # We check if the string "openvpn" is found in the existing connections and use this to consider if the connection was a success or not.
     if sudo netstat -tulpen | grep openvpn >/dev/null ;
     then
