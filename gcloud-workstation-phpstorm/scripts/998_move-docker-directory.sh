1c5c8987215d609408b3367e3719d797f085c395
Mickael Saavedra <mickael.saavedra@strg.at>
2024-05-10 15:30:41 +0200

    feat(gcloud-workstation): Change permissions of docker folder, INPRO-2220

--- gcloud-workstation-phpstorm/scripts/998_move-docker-directory.sh
+++ gcloud-workstation-phpstorm/scripts/998_move-docker-directory.sh
@@ -6,6 +6,7 @@ if [ -d /var/lib/docker ]; then
     pkill -9 containerd # Stop the containerd daemon
     mkdir -p /home/user/docker # Create the new home for docker files
     mv /var/lib/docker/* /home/user/docker/ # Move the contents of /var/lib/docker to /home/user/docker
+    chown -R user:user /home/user/docker # Change the ownership of the new home for docker files
     rm -rf /var/lib/docker # Remove the /var/lib/docker directory
     sed -i 's/\/var\/lib\/docker/\/home\/user\/docker/g' /var/run/docker/containerd/containerd.toml # Update the containerd daemon configuration file
     /usr/bin/dockerd -p /var/run/dockerd.pid --data-root /home/user/docker &>/dev/null & disown # Start the docker daemon in the background
